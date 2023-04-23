ba7a80ed95f80f9f59fd23fef126979fdae240e4
Taras <96896396+tarasnar@users.noreply.github.com>
2023-04-23 19:21:38 +0300

    Add files via upload

--- webviewerlogic.js
+++ webviewerlogic.js
@@ -6,7 +6,8 @@ window.onload = function () {
     const setDropdown = document.getElementById('settings-dropdown');
     const inspDropdown = document.getElementById('inspector-dropdown');
     const viewerCont = document.getElementById('viewer');
-    // const addonsCont = document.getElementById('addons-cont');
+    const canvasCont = document.getElementById('kvas');
+    const bodyCont = document.querySelector('body');
     const btnImage = document.querySelector('.full');
     // When options clicked
     settingsBtn.addEventListener('click', function () {
@@ -31,11 +32,12 @@ window.onload = function () {
     fullBtn.addEventListener('click', function () {
         // If viewer not in full screen scale it
         if (viewerCont.style.height === '64%' && viewerCont.style.width === '52%') {
-            viewerCont.requestFullscreen();
+            bodyCont.requestFullscreen();
             viewerCont.style.height = '100%';
             viewerCont.style.width = '100%';
             viewerCont.style.borderRadius = '0px';
-            // addonsCont.style.display = 'none';
+            canvasCont.style.height = '90%';
+            canvasCont.style.width = '100%';
             btnImage.src = 'ScreenDownsize.svg';
             inspDropdown.style.display = 'none';
             setDropdown.style.display = 'none';
@@ -44,8 +46,9 @@ window.onload = function () {
             document.exitFullscreen();
             viewerCont.style.height = '64%';
             viewerCont.style.width = '52%';
+            canvasCont.style.height = '54%';
+            canvasCont.style.width = '52%';
             viewerCont.style.borderRadius = '10px';
-            // addonsCont.style.display = 'block';
             btnImage.src = 'Fullscreen.svg';
             inspDropdown.style.display = 'none';
             setDropdown.style.display = 'none';
